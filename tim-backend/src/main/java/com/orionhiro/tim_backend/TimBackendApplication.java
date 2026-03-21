d47b11f8f37ed494062e554058dc3ea1b696a580
Marek <ch4ospark@gmail.com>
2026-03-21 16:22:54 +0300

    chore: initial backend project setup

---
+++ tim-backend/src/main/java/com/orionhiro/tim_backend/TimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orionhiro.tim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimBackendApplication.class, args);
+	}
+
+}
