a0eb0b09f0cc196c269ee09adddf4e2f6133ca6b
Marek <ch4ospark@gmail.com>
2026-03-23 20:43:58 +0300

    feat: add task service

--- tim-backend/src/main/java/com/orionhiro/tim_backend/service/TaskService.java
+++ tim-backend/src/main/java/com/orionhiro/tim_backend/service/TaskService.java
@@ -3,9 +3,11 @@ package com.orionhiro.tim_backend.service;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,6 +30,8 @@ public class TaskService {
     private String bucketName;
     @Value("${minio.url}")
     private String minioUrl;
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public Map<String, String> addTask(MultipartFile image) throws BadRequestException{
 
@@ -62,8 +66,16 @@ public class TaskService {
             throw new MediaUploadException(exc.getMessage());
         }
 
+        String taskId = UUID.randomUUID().toString();
+
+        redisTemplate.opsForHash().putAll(
+            taskId, 
+            Map.of("status", "PENDING")
+        );
+        redisTemplate.expire(taskId, 60 * 60, TimeUnit.SECONDS);
+
         return Map.of(
-            "task_id", UUID.randomUUID().toString(),
+            "task_id", taskId,
             "status", "PENDING"
         );
     }
