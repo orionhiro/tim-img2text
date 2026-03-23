fd7e376c77ce0e7435c5370bcd1349f4361d8b6a
Marek <ch4ospark@gmail.com>
2026-03-23 21:02:00 +0300

    refactor: refactor task service method

--- tim-backend/src/main/java/com/orionhiro/tim_backend/service/TaskService.java
+++ tim-backend/src/main/java/com/orionhiro/tim_backend/service/TaskService.java
@@ -1,92 +1,34 @@
 package com.orionhiro.tim_backend.service;
 
-import java.io.InputStream;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.coyote.BadRequestException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.orionhiro.tim_backend.exception.MediaUploadException;
-
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.netty.handler.codec.UnsupportedMessageTypeException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
 
-    private final MinioClient minioClient;
-
-    @Value("${minio.bucket}")
-    private String bucketName;
-    @Value("${minio.url}")
-    private String minioUrl;
-
+    private final ImageService imageService;
     private final RedisTemplate<String, Object> redisTemplate;
 
     public Map<String, String> addTask(MultipartFile image) throws BadRequestException{
-
-        if(image == null || image.isEmpty()){
-            throw new BadRequestException();
-        }
-
-        if(!checkIsImage(image)){
-            throw new UnsupportedMessageTypeException();
-        }
-
-        String image_url = null;
-
-        try{
-            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
-            if (!found) {
-                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-            }
-
-            try (InputStream is = image.getInputStream()) {
-                image_url = UUID.randomUUID().toString() + "." + image.getOriginalFilename().split("\\.")[1];
-                minioClient.putObject(
-                    PutObjectArgs.builder()
-                        .bucket(bucketName)
-                        .object(image_url)
-                        .stream(is, is.available(), -1)
-                        .contentType(image.getContentType())
-                        .build()
-                );
-            }
-        } catch(Exception exc){
-            throw new MediaUploadException(exc.getMessage());
-        }
-
+        
+        String image_url = imageService.saveImage(image);
         String taskId = UUID.randomUUID().toString();
 
-        redisTemplate.opsForHash().putAll(
-            taskId, 
-            Map.of("status", "PENDING")
-        );
+        redisTemplate.opsForHash().putAll(taskId, Map.of("status", "PENDING"));
         redisTemplate.expire(taskId, 60 * 60, TimeUnit.SECONDS);
 
         return Map.of(
             "task_id", taskId,
             "status", "PENDING"
         );
-    }
-
-    private boolean checkIsImage(MultipartFile file){
-        boolean checkContentType = file.getContentType() != null &&
-                  file.getContentType().startsWith("image/");
-        
-        String name = file.getOriginalFilename();
-        boolean checkFileExtension = name != null && name.matches(".*\\.(png|jpg|jpeg|gif|bmp)$");
-
-        return checkContentType && checkFileExtension;
     }
 }
