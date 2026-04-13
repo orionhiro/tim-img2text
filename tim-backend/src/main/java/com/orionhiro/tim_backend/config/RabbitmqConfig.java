49a08b3fdff5fe2e5074c718926b060cb560cba6
Marek <ch4ospark@gmail.com>
2026-04-13 09:37:42 +0300

    chore: remove unused imports

--- tim-backend/src/main/java/com/orionhiro/tim_backend/config/RabbitmqConfig.java
+++ tim-backend/src/main/java/com/orionhiro/tim_backend/config/RabbitmqConfig.java
@@ -5,14 +5,11 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.ContentTypeDelegatingMessageConverter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
