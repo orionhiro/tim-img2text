b49e15a5a4ed8e45e9bcece25460103bf4dd6557
Marek <ch4ospark@gmail.com>
2026-04-13 09:39:34 +0300

    feat: add new method of task controller

--- tim-backend/src/main/java/com/orionhiro/tim_backend/controller/TaskController.java
+++ tim-backend/src/main/java/com/orionhiro/tim_backend/controller/TaskController.java
@@ -4,7 +4,10 @@ import java.util.Map;
 
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import com.orionhiro.tim_backend.service.TaskService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/v1/ocr")
 @RequiredArgsConstructor
 public class TaskController {
     private final TaskService taskService;
@@ -23,5 +27,11 @@ public class TaskController {
     @ResponseStatus(HttpStatus.OK)
     public Map<String, String> addTask(@RequestParam("image") MultipartFile image) throws BadRequestException{
         return taskService.addTask(image);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, String> getTask(@PathVariable("id") String taskId){
+        return taskService.getTask(taskId);
     }
 }
